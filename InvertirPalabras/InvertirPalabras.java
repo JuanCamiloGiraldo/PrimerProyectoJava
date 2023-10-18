722d360f53562b0218caa1289ed4f409ff77731b
JuanCamiloGiraldo <juan_giraldo@itis.com.co>
2023-10-18 16:25:14 -0500

    realicé el primer y segundo ejercicio de Java de los videos

---
+++ InvertirPalabras/InvertirPalabras.java
@@ -0,0 +1,20 @@
+package InvertirPalabras;
+import java.util.Scanner;
+
+public class InvertirPalabras {
+    public static void main (String[]args){
+        String palabra="", palabrainvertida=""; /* se definen los tipos de variables y se declaran */
+        int longitudpalabra =0;
+        Scanner entrada = new Scanner(System.in);   /* Scanner permite leer las entradas del teclado en el sistema */
+        System.out.println("Escribe una palabra o frase: ");
+        palabra = entrada.nextLine();   /* nextline define la entrada como tipo String */
+
+        longitudpalabra = palabra.length();
+
+        while (longitudpalabra != 0) {
+            palabrainvertida += palabra.substring(longitudpalabra -1, longitudpalabra); 
+            longitudpalabra --;
+        }
+        System.out.println(palabrainvertida);
+    }
+};
---
+++ primerjava.java
@@ -0,0 +1,13 @@
+/* no se puede tener más de una clase pública en un proyecto Java */
+public class primerjava{
+
+    /* La clase pública sí o sí se debe llamar como el nombre del fichero, 
+    de otro modo habrá un error de compilación
+     */
+
+}
+class EjemploUno{
+    public static void main(String[] args){
+        System.out.println("hola");
+    }
+}
